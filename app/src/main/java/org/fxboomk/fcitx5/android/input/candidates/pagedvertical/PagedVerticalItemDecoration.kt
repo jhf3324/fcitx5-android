f461817a64cfcc1c2c7c314f88276ddc5b58b72a
jhf3324 <140601511+jhf3324@users.noreply.github.com>
2026-05-05 17:03:44 +0800

    Add PagedVerticalItemDecoration.kt from swan fork

---
+++ app/src/main/java/org/fxboomk/fcitx5/android/input/candidates/pagedvertical/PagedVerticalItemDecoration.kt
@@ -0,0 +1,29 @@
+﻿/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * SPDX-FileCopyrightText: Copyright 2025 Fcitx5 for Android Contributors
+ */
+
+package org.fxboomk.fcitx5.android.input.candidates.pagedvertical
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adds spacing between items in the vertical paged candidates grid.
+ */
+class PagedVerticalItemDecoration(
+    private val spacingPx: Int
+) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.set(spacingPx, spacingPx / 2, spacingPx, spacingPx / 2)
+    }
+}
+
