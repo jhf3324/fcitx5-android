086f689864abdfe1d273025437250791310d0522
jhf3324 <140601511+jhf3324@users.noreply.github.com>
2026-05-05 15:37:51 +0800

    Revert Primitive.kt - styledFloat already exists in Splitties.kt

--- app/src/main/java/org/fxboomk/fcitx5/android/utils/Primitive.kt
+++ app/src/main/java/org/fxboomk/fcitx5/android/utils/Primitive.kt
@@ -1,8 +1,11 @@
-/*  * SPDX-License-Identifier: LGPL-2.1-or-later  * SPDX-FileCopyrightText: Copyright 2024 Fcitx5 for Android Contributors  */  package org.fxboomk.fcitx5.android.utils  import androidx.core.graphics.ColorUtils import kotlin.math.roundToInt  fun Int.alpha(a: Float) = ColorUtils.setAlphaComponent(this, (a * 0xff).roundToInt()) 
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ * SPDX-FileCopyrightText: Copyright 2024 Fcitx5 for Android Contributors
+ */
 
-fun android.content.Context.styledFloat(attr: Int): Float {
-    val ta: android.content.res.TypedArray = theme.obtainStyledAttributes(intArrayOf(attr))
-    val value = ta.getFloat(0, 0.0f)
-    ta.recycle()
-    return value
-}
+package org.fxboomk.fcitx5.android.utils
+
+import androidx.core.graphics.ColorUtils
+import kotlin.math.roundToInt
+
+fun Int.alpha(a: Float) = ColorUtils.setAlphaComponent(this, (a * 0xff).roundToInt())
